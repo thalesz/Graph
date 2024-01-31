53fc1e8214111e880b13eb142b772134ed1250ba
thalesz <brecheret187@gmail.com>
2024-01-31 17:13:40 -0400

    fix Script

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Salva o diretório atual
+current_dir=$(pwd)
+
 # Instala as dependências do projeto React
 echo "Instalando dependências do projeto React..."
 cd Cliente
 npm install  # ou use yarn se preferir
+
+# Retorna ao diretório inicial
+cd "$current_dir"
 
 # Instala as dependências do projeto Node.js
 echo "Instalando dependências do projeto Node.js..."
@@ -11,3 +17,4 @@ cd Servidor/server
 npm install  # ou use yarn se preferir
 
 echo "Instalação concluída em ambos os projetos."
+
